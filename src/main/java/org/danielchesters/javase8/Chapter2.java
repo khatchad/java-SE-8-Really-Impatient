0beeebc87f23e862e7c219fbd8890c890ea8e103
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-07 22:33:39 +0200

    improve Chapter 2 solutions

--- src/main/java/org/danielchesters/javase8/Chapter2.java
+++ src/main/java/org/danielchesters/javase8/Chapter2.java
@@ -11,7 +11,8 @@ import java.util.stream.Stream;
 public class Chapter2 {
 
     //Exercise 4
-    public static void streamOfInt() {
+    public static void exercise4() {
+        System.out.println("Exercise 4");
         int[] values = {1, 4, 9, 16};
         Stream<Object> s = Stream.of(values);
         IntStream intStream = IntStream.of(values);
@@ -20,13 +21,24 @@ public class Chapter2 {
     }
 
     //Exercise 5
-    public static Stream<Long> randomStream(Long a, Long c, Long m, Long seed) {
-        return Stream.iterate(seed, (Long l) -> (a * l + c) % m);
+    public static Stream<Long> randomStream(long a, long c, long m, long seed) {
+        return Stream.iterate(seed, l -> (a * l + c) % m);
+    }
+
+    public static void exercise5() {
+        System.out.println("Exercise 5");
+        Stream<Long> stream = randomStream(25214903917l, 11, (long) Math.pow(2, 48), System.currentTimeMillis());
+        stream.limit(5).forEach(System.out::println);
     }
 
     //Exercise 6
     public static Stream<Character> characterStream(String s) {
         return IntStream.rangeClosed(0, s.length() - 1).mapToObj(s::charAt);
+    }
+
+    public static void exercise6(){
+        System.out.println("Exercise 6");
+        characterStream("toto").forEach(System.out::println);
     }
 
     //Exercise 7 (reflection)
@@ -49,12 +61,16 @@ public class Chapter2 {
     }
 
     public static void exercise8() {
+        System.out.println("Exercise 8");
         Stream<Integer> first = Stream.of(1, 2, 3, 4, 5, 6);
         Stream<Integer> second = Stream.of(1, 2, 3, 4, 5, 6, 7);
         zip(first, second).forEach(System.out::println);
     }
 
     public static void main(String... args) {
+        exercise4();
+        exercise5();
+        exercise6();
         exercise8();
     }
 }
