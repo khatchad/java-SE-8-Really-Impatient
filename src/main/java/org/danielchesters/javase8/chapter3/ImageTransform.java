c0753557ef000da48000619044371f0dcb20981d
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-09 01:52:34 +0200

    Exercise 12 Chapter 3

--- src/main/java/org/danielchesters/javase8/chapter3/ImageTransform.java
+++ src/main/java/org/danielchesters/javase8/chapter3/ImageTransform.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.UnaryOperator;
 
@@ -18,6 +20,44 @@ import java.util.function.UnaryOperator;
 @FunctionalInterface
 interface ColorTransformer {
     Color apply(int x, int y, Color colorAtXY);
+}
+
+//Exercise 12
+class LatentImage {
+    private Image in;
+    private List<ColorTransformer> pendingOperations;
+
+    public static LatentImage from(Image in) {
+        LatentImage result = new LatentImage();
+        result.in = in;
+        result.pendingOperations = new ArrayList<>();
+        return result;
+    }
+
+    LatentImage transform(UnaryOperator<Color> f) {
+        pendingOperations.add((x,y,c) -> f.apply(c));
+        return this;
+    }
+
+    LatentImage transform(ColorTransformer f) {
+        pendingOperations.add(f);
+        return this;
+    }
+
+    public Image toImage() {
+        int width = (int) in.getWidth();
+        int height = (int) in.getHeight();
+        WritableImage out = new WritableImage(width, height);
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                Color c = in.getPixelReader().getColor(x, y);
+                for (ColorTransformer f : pendingOperations) {
+                    c = f.apply(x, y, c);
+                }
+                out.getPixelWriter().setColor(x, y, c);
+            }
+        return out;
+    }
 }
 
 //Exercise 5
