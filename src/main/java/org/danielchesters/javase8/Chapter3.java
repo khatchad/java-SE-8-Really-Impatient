44029291c34770bc62d57935f41d5b7e1be7406c
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-08 18:40:03 +0200

    Exercise 1 Chapter 3

---
+++ src/main/java/org/danielchesters/javase8/Chapter3.java
@@ -0,0 +1,30 @@
+package org.danielchesters.javase8;
+
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by daniel on 08/05/14.
+ */
+public class Chapter3 {
+
+    //Exercise 1
+    public static void logIf(Level level, Supplier<Boolean> condition, Supplier<String> message) {
+        Logger logger = Logger.getGlobal();
+        if (logger.isLoggable(level)) {
+            if (condition.get()) {
+                logger.log(level, message.get());
+            }
+        }
+    }
+
+    public static void exercise1() {
+        System.out.println("Exercice 1");
+        logIf(Level.INFO, () -> true, () -> "Test");
+    }
+
+    public static void main(String... args) {
+        exercise1();
+    }
+}
