6226eefdfc40cfbb358267a20edc832cd346d07a
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-09 01:15:52 +0200

    Exercise 9 Chapter 3

--- src/main/java/org/danielchesters/javase8/Chapter3.java
+++ src/main/java/org/danielchesters/javase8/Chapter3.java
@@ -1,6 +1,10 @@
 package org.danielchesters.javase8;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 import java.util.logging.Level;
@@ -67,9 +71,52 @@ public class Chapter3 {
         };
     }
 
+    //Exercise 9 : I suppose that fields are String
+    public static Comparator<Object> lexicographicComparator(String... fieldNames) {
+        return (Object o1, Object o2) -> {
+            int result = 0;
+            try {
+                for (String fieldName : fieldNames) {
+                    Field field = o1.getClass().getDeclaredField(fieldName);
+                    field.setAccessible(true);
+                    String field1 = (String) field.get(o1);
+                    String field2 = (String) field.get(o2);
+                    result = field1.compareTo(field2);
+                    if (result != 0){
+                        break;
+                    }
+                }
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return result;
+        };
+    }
+
+    static class Test {
+        String lastName;
+        String firstName;
+        public Test(String lastName, String firstName) {
+            this.lastName = lastName;
+            this.firstName = firstName;
+        }
+        public String toString() {
+            return firstName + " " + lastName;
+        }
+    }
+
+    public static void exercise9() {
+        System.out.println("Exercise 9");
+
+        List<Test> tests = Arrays.asList(new Test("Test","Test"), new Test("Chesters", "Daniel"));
+        tests.sort(lexicographicComparator("lastName", "firstName"));
+        tests.forEach(System.out::println);
+    }
+
     public static void main(String... args) {
         exercise1();
         exercise2();
-        exercise3();
+        //I am not run always exercise 3 method, because it is throw an error 50% of time
+        exercise9();
     }
 }
