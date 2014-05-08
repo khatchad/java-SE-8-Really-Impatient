ab40495f194ba2247c73a5e3c6f08a09ea7a2c13
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-08 23:02:34 +0200

    Exercise 6 Chapter 3

--- src/main/java/org/danielchesters/javase8/org/danielchesters/javase8/chapter3/ImageTransform.java
+++ src/main/java/org/danielchesters/javase8/org/danielchesters/javase8/chapter3/ImageTransform.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.function.BiFunction;
 import java.util.function.UnaryOperator;
 
 /**
@@ -19,9 +20,10 @@ interface ColorTransformer {
     Color apply(int x, int y, Color colorAtXY);
 }
 
+//Exercise 5
 //Ctrl-c Ctrl-v from source code of book
 //The solution is in from source code of Chapter 3 section 3 on the website of the author
-public class Exercise5 extends Application {
+public class ImageTransform extends Application {
 
     public static Image transform(Image in, UnaryOperator<Color> f) {
         int width = (int) in.getWidth();
@@ -47,6 +49,19 @@ public class Exercise5 extends Application {
         return out;
     }
 
+    //Exercise6
+    public static <T> Image transform(Image in, BiFunction<Color, T, Color> f, T arg) {
+        int width = (int) in.getWidth();
+        int height = (int) in.getHeight();
+        WritableImage out = new WritableImage(
+                width, height);
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                out.getPixelWriter().setColor(x, y,
+                        f.apply(in.getPixelReader().getColor(x, y), arg));
+        return out;
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Image image = new Image(this.getClass().getClassLoader().getResourceAsStream("queen-mary.png"));
@@ -56,7 +71,8 @@ public class Exercise5 extends Application {
                         || y < 10 || y > image.getHeight() - 10 ? Color.GRAY : c
         );
 
-        stage.setScene(new Scene(new HBox(new ImageView(image), new ImageView(brightenedImage), new ImageView(image2))));
+        Image image3 = transform(image, (Color c, Double d) -> c.deriveColor(0, 1, d, 1), 1.2);
+        stage.setScene(new Scene(new HBox(new ImageView(image), new ImageView(brightenedImage), new ImageView(image2), new ImageView(image3))));
         stage.show();
     }
 }
