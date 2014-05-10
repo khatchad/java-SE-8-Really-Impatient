59f69ee0dc5117aa401ed78a0057104d03302f3a
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-10 19:00:33 +0200

    Exercise 4 Chapter 5

--- src/main/java/org/danielchesters/javase8/Chapter5.java
+++ src/main/java/org/danielchesters/javase8/Chapter5.java
@@ -1,10 +1,11 @@
 package org.danielchesters.javase8;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.function.Predicate;
 
 /**
@@ -36,13 +37,13 @@ public class Chapter5 {
 
     //Exercise 3
     public static TemporalAdjuster next(Predicate<LocalDate> predicate) {
-        return w -> {
-            LocalDate date = (LocalDate) w;
+        return TemporalAdjusters.ofDateAdjuster(w -> {
+            LocalDate date = w;
             do {
                 date = date.plusDays(1);
             } while (predicate.test(date));
             return date;
-        };
+        });
     }
 
     public static void exercise3() {
@@ -50,9 +51,41 @@ public class Chapter5 {
         System.out.println(LocalDate.now().with(next(d -> d.getDayOfWeek().getValue() < 6)));
     }
 
+    //Exercise 4 (need improve for the weeks management)
+    public static void cal(int month, int year) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        LocalDate firstDayOfNextMonth = firstDayOfMonth.with(TemporalAdjusters.firstDayOfNextMonth());
+        LocalDate dateLoop = firstDayOfMonth;
+        int[][] dates = new int[7][6];
+        while (dateLoop.isBefore(firstDayOfNextMonth)) {
+            dates[dateLoop.getDayOfWeek().getValue() - 1][dateLoop.get(WeekFields.ISO.weekOfMonth())] = dateLoop.getDayOfMonth();
+            dateLoop = dateLoop.plusDays(1);
+        }
+
+        for (int w = 0; w < 6; w++) {
+            for (int d = 0; d < 7; d++) {
+                int i = dates[d][w];
+                if (i == 0) {
+                    System.out.print("   ");
+                } else {
+                    System.out.printf("%2d ", i);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void exercise4() {
+        System.out.println("Exercise 4");
+        cal(3, 2013);
+        cal(5, 2014);
+    }
+
     public static void main(String... args) {
         exercise1();
         exercise2();
         exercise3();
+        exercise4();
     }
 }
