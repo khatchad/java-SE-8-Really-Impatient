2440d336880d566c288f964253799e445743b67c
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-17 20:25:27 +0200

    Exercise 2 Chapter 8

---
+++ src/main/java/org/danielchesters/javase8/Chapter8.java
@@ -0,0 +1,16 @@
+package org.danielchesters.javase8;
+
+/**
+ * Created by daniel on 17/05/14.
+ */
+public class Chapter8 {
+    //Exercise 2
+    public static void exercise2() {
+        System.out.println("Exercise 2");
+        System.out.println(Math.negateExact(Integer.MIN_VALUE));
+    }
+
+    public static void main(String... args) {
+        exercise2();
+    }
+}
