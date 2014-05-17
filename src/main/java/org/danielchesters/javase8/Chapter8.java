e309f197c29d0b212f9822910c042ee83ff7001b
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-17 20:34:23 +0200

    Exercise 3 Chapter 8

--- src/main/java/org/danielchesters/javase8/Chapter8.java
+++ src/main/java/org/danielchesters/javase8/Chapter8.java
@@ -7,10 +7,47 @@ public class Chapter8 {
     //Exercise 2
     public static void exercise2() {
         System.out.println("Exercise 2");
-        System.out.println(Math.negateExact(Integer.MIN_VALUE));
+        //System.out.println(Math.negateExact(Integer.MIN_VALUE));
+    }
+
+    //Exercise 3
+    public static int gcd1(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd1(b, a % b);
+        }
+    }
+
+    public static int gcd2(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+          return gcd2(b, Math.floorMod(a,b));
+        }
+    }
+
+    public static int gcd3(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd3(b, rem(a, b));
+        }
+    }
+
+    private static int rem(int a, int b) {
+        return Integer.remainderUnsigned(a, b);
+    }
+
+    public static void exercise3() {
+        System.out.println("Exercise 3");
+        System.out.println(gcd1(20, -2));
+        System.out.println(gcd2(20, -2));
+        System.out.println(gcd3(20, -2));
     }
 
     public static void main(String... args) {
         exercise2();
+        exercise3();
     }
 }
